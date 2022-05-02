e09d3ad684cf85e9bf23193d76bd1f8fb410062e
LachlanSchulze <19806@stpauls.qld.edu.au>
2022-05-02 21:47:42 +1000

    ls

--- FlaskApp1/routes.py
+++ FlaskApp1/routes.py
@@ -1,30 +1,29 @@
 from FlaskApp1 import app
-from flask import render_template, request, json, Response
+from flask import render_template, request, json, Response, redirect, flash
 from FlaskApp1.models import User, Course, Enrolment
-
-courseData = [
-    {"courseID": "1111", "title": "PHP 101", "description": "Intro to PHP", "credits": 3, "term": "Fall, Spring"},
-    {"courseID": "2222", "title": "Java 1", "description": "Intro to Java Programming", "credits": 4,
-     "term": "Spring"},
-    {"courseID": "3333", "title": "Adv PHP 201", "description": "Advanced PHP Programming", "credits": 3,
-     "term": "Fall"},
-    {"courseID": "4444", "title": "Angular 1", "description": "Intro to Angular", "credits": 3,
-     "term": "Fall, Spring"},
-    {"courseID": "5555", "title": "Java 2", "description": "Advanced Java Programming", "credits": 4,
-     "term": "Fall"}]
-
+from FlaskApp1.forms import LoginForm, RegisterForm
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
     # return "<h1>This is Boring</h1>"
-    return render_template("index.html", login=False, index=True)
+    return render_template("index.html", index=True)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.objects(email=email).first()
+        if user and password == user.password:
+            flash(f"{user.first_name}, you are successfully logged in!", "success")
+            return redirect("/index")
+        else:
+            flash("Sorry, something went wrong.", "danger")
+    return render_template("login.html", form=form, title="Login", login=True)
 
 
 @app.route("/courses")
